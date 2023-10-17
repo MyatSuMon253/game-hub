8256462aec4f41a2c2899fa09399e526ca0ad3c5
Myat Su Mon <myatsumon2531999@gmail.com>
2023-10-17 23:22:00 +0630

    feat: fetching platforms

--- src/hooks/usePlatforms.ts
+++ src/hooks/usePlatforms.ts
@@ -1,11 +1,23 @@
+import { useQuery } from "@tanstack/react-query";
 import platforms from "../data/platforms";
+import apiClient from "../services/api-client";
+import { FetchResponse } from "./useData";
 
-interface Platform {
+export interface Platform {
   id: number;
   name: string;
   slug: string;
 }
 
-const usePlatforms = () => ({ data: platforms, isLoading: false, error: null });
+const usePlatforms = () =>
+  useQuery({
+    queryKey: ["platforms"],
+    queryFn: () =>
+      apiClient
+        .get<FetchResponse<Platform>>("/platforms/list/parents")
+        .then((res) => res.data),
+    staleTime: 1000 * 60 * 60 * 24,
+    initialData: { count: platforms.length, results: platforms },
+  });
 
 export default usePlatforms;
